8def0ff241810f4299d7fdfaf19f77f019de4c6c
Alexey Zyukov <aalex64@ukr.net>
2022-01-26 21:59:09 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
-  persistStore,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -8,10 +7,8 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import contactsReducer from './contacts-reducer';
-import { contactsAPI } from './contactsSlice';
 
 const middleware = [
   ...getDefaultMiddleware({
@@ -22,20 +19,12 @@ const middleware = [
   logger,
 ];
 
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-//   blacklist: ['filter'],
-// };
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
-    [contactsAPI.reducerPath]: contactsAPI.reducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
-
-export default { store, persistor };
+export default { store };
