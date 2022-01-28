5fc47c1fd69ced2477254a1f259815b101bfffb4
Alexey Zyukov <aalex64@ukr.net>
2022-01-28 21:10:25 +0200

    update

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,24 +1,32 @@
 import { createAction } from '@reduxjs/toolkit';
+import { ADD, DELETE, FILTER } from './types';
 
-const asyncGetContacts = createAction('contacts/get');
-export default { asyncGetContacts };
+const addContact = createAction(ADD);
 
-export const fetchContactRequest = createAction('contacts/fetchContactRequest');
-export const fetchContactSuccess = createAction('contacts/fetchContactSuccess');
-export const fetchContactError = createAction('contacts/fetchContactError');
+const deleteContact = createAction(DELETE);
+const filterContacts = createAction(FILTER);
 
-export const addContactRequest = createAction('contacts/addContactRequest');
-export const addContactSuccess = createAction('contacts/addContactSuccess');
-export const addContactError = createAction('contacts/addContactError');
-
-export const delContactRequest = createAction('contacts/delContactRequest');
-export const delContactSuccess = createAction('contacts/delContactSuccess');
-export const delContactError = createAction('contacts/delContactError');
-
-export const delContacts = createAction('contacts/DELETE');
-export const filterContacts = createAction('contacts/FILTER');
+export default { addContact, filterContacts };
 
 // import { createAction } from '@reduxjs/toolkit';
+
+// const asyncGetContacts = createAction('contacts/get');
+// export default { asyncGetContacts };
+
+// export const fetchContactRequest = createAction('contacts/fetchContactRequest');
+// export const fetchContactSuccess = createAction('contacts/fetchContactSuccess');
+// export const fetchContactError = createAction('contacts/fetchContactError');
+
+// export const addContactRequest = createAction('contacts/addContactRequest');
+// export const addContactSuccess = createAction('contacts/addContactSuccess');
+// export const addContactError = createAction('contacts/addContactError');
+
+// export const delContactRequest = createAction('contacts/delContactRequest');
+// export const delContactSuccess = createAction('contacts/delContactSuccess');
+// export const delContactError = createAction('contacts/delContactError');
+
+// export const delContacts = createAction('contacts/DELETE');
+// export const filterContacts = createAction('contacts/FILTER');
 
 // export const fetchContactRequest = createAction('contacts/fetchContactRequest');
 // export const fetchContactSuccess = createAction('contacts/fetchContactSuccess');
@@ -27,11 +35,3 @@ export const filterContacts = createAction('contacts/FILTER');
 // export const deleteContact = createAction('contacts/DELETE');
 // export const filterContacts = createAction('contacts/FILTER');
 //=======================================
-// import { ADD, DELETE, FILTER } from './types';
-
-// const addContact = createAction(ADD);
-
-// const deleteContact = createAction(DELETE);
-// const filterContacts = createAction(FILTER);
-
-// export default { addContact, filterContacts };
